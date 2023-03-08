b18cf96d8eaa66e1a1e321541abca65c23cb2fee
makennawold <makennawold@gmail.com>
2023-03-07 18:16:07 -0600

    added stitch counter page and react router dom at v5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./styles/app.css";
 import Counter from "./features/counter/Counter";
+import StitchCounter from "./features/stitchcounter/StitchCounter";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Counter />
+    <div className="app" style={{ display: "flex", flexDirection: "column" }}>
+      <Router>
+        <div className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/counter">Counter</Link>
+          <Link to="/stitchcounter">StitchCounter</Link>
+        </div>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              Home
+            </Route>
+            <Route path="/stitchcounter">
+              <StitchCounter />
+            </Route>
+            <Route path="/counter">
+              <Counter />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/features/stitchcounter/StitchCounter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StitchCounter = () => {
+  return <div>this is my stitch counter page</div>;
+};
+
+export default StitchCounter;
