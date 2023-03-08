5407b30fdeeaedbec77f468e996271b4e2b757a9
makennawold <makennawold@gmail.com>
2023-03-07 18:00:16 -0600

    added reset and increment by amount

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,15 +1,38 @@
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+
+import { useState } from "react";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState(0);
+
+  const addValue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
+
   return (
     <div>
       <p>{count}</p>
       <div>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+
+        <input
+          type="text"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>reset</button>
       </div>
     </div>
   );
